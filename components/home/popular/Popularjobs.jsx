fe4881b4f9c8819e9e184f8709ec9cf2de65d750
Raffaele Marcello <marcelloraffaele@gmail.com>
2024-02-11 23:08:28 +0100

    popularjobs ui ok

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,10 +1,17 @@
-import {useState} from 'react'
-import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native'
-import {useRouter} from 'expo-router';
-import { styles } from './popularjobs.style'
-import styles from './popularjobs.style'
-import { COLORS, SIZES } from '../../../constants';
-import PopularJobCards from '../../common/cards/popular/PopularJobCard'
+import { useState } from "react";
+import { useRouter } from "expo-router";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
+
+import styles from "./popularjobs.style";
+import { COLORS, SIZES } from "../../../constants";
+import PopularJobCard from "../../common/cards/popular/PopularJobCard";
+//import useFetch from "../../../hook/useFetch";
 
 const Popularjobs = () => {
   const router = useRouter();
@@ -14,27 +21,28 @@ const Popularjobs = () => {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Text style={styles.headerTitle}>Popularjobs</Text>
+        <Text style={styles.headerTitle}>Popular jobs</Text>
         <TouchableOpacity>
-        <Text style={styles.headerBtn}>Show all</Text>
+          <Text style={styles.headerBtn}>Show all</Text>
         </TouchableOpacity>
       </View>
 
       <View style={styles.cardsContainer}>
-        { isLoading ? (
-          <ActivityIndicator size="large" colors={COLORS.primary} />
+        {isLoading ? (
+          <ActivityIndicator size='large' color={COLORS.primary} />
         ) : error ? (
           <Text>Something went wrong</Text>
         ) : (
-          <FlatList 
-            data={[1,2,3,4,5]}
-            renderItem={(item)=> (
-              <PopularJobCards
+          <FlatList
+            data={[1,2,3,4]}
+            renderItem={({ item }) => (
+              <PopularJobCard
                 item={item}
+                
               />
             )}
-            keyExtractor={item => item?.job_id}
-            contentContainer Style={{ columnGap: SIZES.medium}}
+            keyExtractor={(item) => item?.job_id}
+            contentContainerStyle={{ columnGap: SIZES.medium }}
             horizontal
           />
         )}
